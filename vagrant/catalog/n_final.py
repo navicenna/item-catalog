55ed932d7bffe46178ab342d9ff12bdc3885e7dc
n-del <nbahmanyar@zoho.com>
2018-05-23 17:34:27 -0400

    new

--- vagrant/catalog/n_final.py
+++ vagrant/catalog/n_final.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
-from sqlalchemy import create_engine
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from n_database_setup import Base, Project, Item
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
 
 app = Flask(__name__)
 
@@ -45,7 +55,7 @@ def showProjects():
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in xrange(32))
+                    for x in range(32))
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
