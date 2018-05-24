bffece6f28cb61408b53e2efcd65f7636ab39076
n-del <nbahmanyar@zoho.com>
2018-05-24 17:54:23 -0400

    adding oauth

--- vagrant/catalog/n_final.py
+++ vagrant/catalog/n_final.py
@@ -11,6 +11,10 @@ import httplib2
 import json
 from flask import make_response
 import requests
+from flask_oauthlib.client import OAuth
+
+
+# Some code adapted from this flask-oauth example https://github.com/lepture/flask-oauthlib/blob/master/example/google.py
 
 
 app = Flask(__name__)
@@ -22,6 +26,58 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
+# Configure the OAUTH parameters
+app.config['GOOGLE_ID'] = "825349126192-ujidiia6lfn8vok2o1ep879r37qlrumv.apps.googleusercontent.com"
+app.config['GOOGLE_SECRET'] = "mxi8yfVHvvtTDR8fSdFGesAm"
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+
+google = oauth.remote_app(
+    'google',
+    consumer_key=app.config.get('GOOGLE_ID'),
+    consumer_secret=app.config.get('GOOGLE_SECRET'),
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
+
+# Login
+@app.route('/login')
+def login():
+    return google.authorize(callback=url_for('authorized', _external=True))
+
+
+@app.route('/logout')
+def logout():
+    login_session.pop('google_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def authorized():
+    resp = google.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    login_session['google_token'] = (resp['access_token'], '')
+    me = google.get('userinfo')
+    return jsonify({"data": me.data})
+
+
+@google.tokengetter
+def get_google_oauth_token():
+    return login_session.get('google_token')
+
+
+# Build out the JSON API endpoints
 @app.route('/project/<int:project_id>/items/JSON')
 def ProjectJSON(project_id):
     project = session.query(Project).filter_by(id=project_id).one()
@@ -47,18 +103,8 @@ def allProjectsJSON():
 @app.route('/project/')
 def showProjects():
     projects = session.query(Project).all()
-    # return "This page will show all my restaurants"
     return render_template('catalog-main.html', projects=projects)
 
-
-# Create anti-forgery state token
-@app.route('/login')
-def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in range(32))
-    login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
-    return render_template('login.html', STATE=state)
 
 
 # # Create a new restaurant
